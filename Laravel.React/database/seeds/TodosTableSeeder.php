2121a374884af5b44e82e7de0f5fffb1ed8d786f
Masatoshi <msts.oo0131@gmail.com>
2021-02-15 18:19:33 +0900

    seed_migrated

---
+++ Laravel.React/database/seeds/TodosTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TodosTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('todos')->truncate();
+        DB::table('todos')->insert([
+            ['memo' => '1234', 'status' => 0]
+        ]);
+    }
+}
--- Laravel.React/routes/web.php
+++ Laravel.React/routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Route::post('/register','')->name('register');
+
 Route::get('/', function () {
     return view('welcome');
 });
