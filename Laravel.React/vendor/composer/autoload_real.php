cac2e65b328fac907143088615b4a55412bdbb81
Masatoshi <msts.oo0131@gmail.com>
2021-03-20 18:40:39 +0900

    composer_reinstall

--- Laravel.React/vendor/composer/autoload_real.php
+++ Laravel.React/vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInitc27bbb7ed7b1cdf3b8be7059945dff09
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInitc27bbb7ed7b1cdf3b8be7059945dff09', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInitc27bbb7ed7b1cdf3b8be7059945dff09', 'loadClassLoader'));
@@ -52,24 +50,6 @@ class ComposerAutoloaderInitc27bbb7ed7b1cdf3b8be7059945dff09
 
         $loader->register(true);
 
-        if ($useStaticLoader) {
-            $includeFiles = Composer\Autoload\ComposerStaticInitc27bbb7ed7b1cdf3b8be7059945dff09::$files;
-        } else {
-            $includeFiles = require __DIR__ . '/autoload_files.php';
-        }
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequirec27bbb7ed7b1cdf3b8be7059945dff09($fileIdentifier, $file);
-        }
-
         return $loader;
-    }
-}
-
-function composerRequirec27bbb7ed7b1cdf3b8be7059945dff09($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        require $file;
-
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
