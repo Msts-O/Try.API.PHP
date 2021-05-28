71994fd92c4c5bcff4b6613ce4f9110893d88255
Masatoshi <msts.oo0131@gmail.com>
2021-05-28 14:56:52 +0900

    template_layout_create

---
+++ bbs/resources/views/layouts/header.blade.php
@@ -0,0 +1,7 @@
+@section('header')
+    <header class="navbar navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="{{ url('/bbs/') }}">What's your news?</a>
+        </div>
+    </header>
+@endsection
